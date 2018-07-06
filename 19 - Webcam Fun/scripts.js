218039f7b64d64de9e5a9e1a96898c190f093498
Cesar Level <cesarlevel@gmail.com>
2018-07-06 08:34:24 -0400

    Working with canvas

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -8,8 +8,48 @@ function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
       console.log(localMediaStream);
-      video.src = Window.URL.createObjectURL(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(error => {
+      console.error(error);
     });
 }
 
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = redEffect(pixels);
+  }, 16)
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src='${data}' alt="Handsome Man" />`;
+  //link.textContent = 'Download Image';
+  strip.insertBefore(link, strip.firstChild)
+}
+
+function redEffect(pixels){
+  for(let i = 0; i < pixels.length; i += 4){
+    pixels[i + 0]; // R
+    pixels[i + 1]; // G
+    pixels[i + 2]; // B
+
+  }
+}
+
 getVideo();
+video.addEventListener('canplay', paintToCanvas);
